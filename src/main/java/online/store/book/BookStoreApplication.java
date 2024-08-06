a5409b93b207dbc4ae76fab9005c20c5d9e24a02
Vitaliy <vitaliyfilimonov3@gmail.com>
2024-08-06 11:42:52 +0300

    checkstyle check

--- src/main/java/online/store/book/BookStoreApplication.java
+++ src/main/java/online/store/book/BookStoreApplication.java
@@ -1,4 +1,4 @@
-package online_store.book;
+package online.store.book;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class BookStoreApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(BookStoreApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(BookStoreApplication.class, args);
+    }
 }
--- src/test/java/online/store/book/BookStoreApplicationTests.java
+++ src/test/java/online/store/book/BookStoreApplicationTests.java
@@ -1,4 +1,4 @@
-package online_store.book;
+package online.store.book;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
