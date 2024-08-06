98c14cc4389ccb7c4e00eaf7536b7aeabcdb17be
Vitaliy <vitaliyfilimonov3@gmail.com>
2024-08-06 10:59:24 +0300

    initial commit

---
+++ src/main/java/online_store/book/BookStoreApplication.java
@@ -0,0 +1,13 @@
+package online_store.book;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookStoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/online_store/book/BookStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package online_store.book;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
