f698148f27d56c77895cfdc9526e15e3907a8a31
Vitaliy <vitaliyfilimonov3@gmail.com>
2024-08-06 12:50:57 +0300

    created Book entity

---
+++ src/main/java/online/store/book/model/Book.java
@@ -0,0 +1,27 @@
+package online.store.book.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+import java.math.BigDecimal;
+
+@Entity
+@Data
+public class Book {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(nullable = false)
+    private String title;
+    @Column(nullable = false)
+    private String author;
+    @Column(nullable = false, unique = true)
+    private String isbn;
+    @Column(nullable = false)
+    private BigDecimal price;
+    private String description;
+    private String coverImage;
+}
