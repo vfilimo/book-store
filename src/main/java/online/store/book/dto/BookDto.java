c639938ddd9a4142bd5c8384d95e776078498b34
Vitaliy <vitaliyfilimonov3@gmail.com>
2024-08-11 12:02:05 +0300

    created BookDto and mapper

---
+++ src/main/java/online/store/book/dto/BookDto.java
@@ -0,0 +1,17 @@
+package online.store.book.dto;
+
+import java.math.BigDecimal;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class BookDto {
+    private Long id;
+    private String title;
+    private String author;
+    private String isbn;
+    private BigDecimal price;
+    private String description;
+    private String coverImage;
+}
