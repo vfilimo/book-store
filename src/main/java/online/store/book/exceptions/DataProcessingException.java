3151c89fc861599ecc0ba7a274af0c83add38542
Vitaliy <vitaliyfilimonov3@gmail.com>
2024-08-27 14:55:44 +0300

    removed unused DataProcessingException

--- src/main/java/online/store/book/exceptions/DataProcessingException.java
+++
@@ -1,7 +0,0 @@
-package online.store.book.exceptions;
-
-public class DataProcessingException extends RuntimeException {
-    public DataProcessingException(String message, Throwable cause) {
-        super(message, cause);
-    }
-}
