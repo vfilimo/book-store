4484e145dd3e5f218858a27505592fd8f905bb26
Vitaliy <vitaliyfilimonov3@gmail.com>
2024-08-29 08:37:22 +0300

    added default pageable values

--- src/main/java/online/store/book/controller/BookController.java
+++ src/main/java/online/store/book/controller/BookController.java
@@ -10,6 +10,7 @@ import online.store.book.dto.BookSearchParameters;
 import online.store.book.dto.CreateBookRequestDto;
 import online.store.book.service.BookService;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -24,18 +25,25 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/books")
 @RequiredArgsConstructor
 public class BookController {
+    private static final int DEFAULT_PAGE = 0;
+    private static final int DEFAULT_PAGE_SIZE = 0;
+    private static final String DEFAULT_SORT_PARAMETER = "price";
     private final BookService bookService;
 
     @Operation(summary = "Get all books", description = "Get all books")
     @GetMapping
-    public List<BookDto> getAll(Pageable pageable) {
+    public List<BookDto> getAll(
+            @PageableDefault(size = DEFAULT_PAGE_SIZE, page = DEFAULT_PAGE,
+                    sort = DEFAULT_SORT_PARAMETER) Pageable pageable) {
         return bookService.findAll(pageable);
     }
 
     @Operation(summary = "Get all books by search parameters",
             description = "Get all books by tittle and author")
     @GetMapping("/search")
-    public List<BookDto> search(BookSearchParameters bookSearchParameters, Pageable pageable) {
+    public List<BookDto> search(BookSearchParameters bookSearchParameters,
+                                @PageableDefault(size = DEFAULT_PAGE_SIZE, page = DEFAULT_PAGE,
+                                        sort = DEFAULT_SORT_PARAMETER) Pageable pageable) {
         return bookService.search(bookSearchParameters, pageable);
     }
 
