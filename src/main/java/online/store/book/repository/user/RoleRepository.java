10aeacfa640833c1ee10bcf84645b7596e91569b
Vitaliy <vitaliyfilimonov3@gmail.com>
2024-09-07 14:12:43 +0300

    added Role entity with changelog and repository

---
+++ src/main/java/online/store/book/repository/user/RoleRepository.java
@@ -0,0 +1,9 @@
+package online.store.book.repository.user;
+
+import online.store.book.model.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RoleRepository extends JpaRepository<Role, Long> {
+}
