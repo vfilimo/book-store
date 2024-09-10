169e006323aa0ab03c3b6b98e86ed1d92d54a7cc
Vitaliy <vitaliyfilimonov3@gmail.com>
2024-09-10 16:14:15 +0300

    added login endpoint

--- src/main/java/online/store/book/controller/AuthenticationController.java
+++ src/main/java/online/store/book/controller/AuthenticationController.java
@@ -4,9 +4,12 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import online.store.book.dto.user.UserLoginRequestDto;
+import online.store.book.dto.user.UserLoginResponseDto;
 import online.store.book.dto.user.UserRegistrationRequestDto;
 import online.store.book.dto.user.UserResponseDto;
 import online.store.book.exceptions.RegistrationException;
+import online.store.book.security.AuthenticationService;
 import online.store.book.service.user.UserService;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -19,11 +22,18 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/auth")
 public class AuthenticationController {
     private final UserService userService;
+    private final AuthenticationService authenticationService;
 
     @Operation(summary = "Save new user")
     @PostMapping("/registration")
     public UserResponseDto register(@RequestBody @Valid UserRegistrationRequestDto requestUser)
             throws RegistrationException {
         return userService.register(requestUser);
+    }
+
+    @Operation(summary = "Login user")
+    @PostMapping("/login")
+    public UserLoginResponseDto login(@RequestBody @Valid UserLoginRequestDto requestDto) {
+        return authenticationService.authentication(requestDto);
     }
 }
