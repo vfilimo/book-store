117e7ab95a4619557beb9d6636cc4f4019cfc1d9
Vitaliy <vitaliyfilimonov3@gmail.com>
2024-09-11 12:44:54 +0300

    refactor BookRepository and CategoryRepository

--- src/main/java/online/store/book/repository/book/BookRepository.java
+++ src/main/java/online/store/book/repository/book/BookRepository.java
@@ -4,10 +4,13 @@ import java.util.List;
 import online.store.book.model.Book;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface BookRepository extends JpaRepository<Book, Long>,
         JpaSpecificationExecutor<Book> {
-    List<Book> findAllByCategoryId(Long categoryId);
+    @Query("SELECT b FROM Book b JOIN b.categories c WHERE c.id = :categoryId")
+    List<Book> findAllByCategoryId(@Param("categoryId") Long categoryId);
 }
