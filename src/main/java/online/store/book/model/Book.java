ea5cffcff1193807f5506afa492c6b6f36358fed
Vitaliy <vitaliyfilimonov3@gmail.com>
2024-09-11 12:13:13 +0300

    code refactor

--- src/main/java/online/store/book/model/Book.java
+++ src/main/java/online/store/book/model/Book.java
@@ -12,9 +12,10 @@ import jakarta.persistence.Table;
 import java.math.BigDecimal;
 import java.util.HashSet;
 import java.util.Set;
-
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
+import lombok.ToString;
 import org.hibernate.annotations.SQLDelete;
 import org.hibernate.annotations.SQLRestriction;
 
@@ -46,5 +47,7 @@ public class Book {
             joinColumns = @JoinColumn(name = "book_id"),
             inverseJoinColumns = @JoinColumn(name = "category_id")
     )
+    @ToString.Exclude
+    @EqualsAndHashCode.Exclude
     private Set<Category> categories = new HashSet<>();
 }
--- src/main/java/online/store/book/repository/book/BookRepository.java
+++ src/main/java/online/store/book/repository/book/BookRepository.java
@@ -1,11 +1,10 @@
 package online.store.book.repository.book;
 
+import java.util.List;
 import online.store.book.model.Book;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
-
-import java.util.List;
 
 @Repository
 public interface BookRepository extends JpaRepository<Book, Long>,
