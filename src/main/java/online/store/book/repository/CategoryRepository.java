df8739ade90202dd542fb3b7dab93057829b60f8
Vitaliy <vitaliyfilimonov3@gmail.com>
2024-09-11 12:44:54 +0300

    refactor BookRepository and CategoryRepository

--- src/main/java/online/store/book/repository/CategoryRepository.java
+++ src/main/java/online/store/book/repository/CategoryRepository.java
@@ -2,6 +2,8 @@ package online.store.book.repository;
 
 import online.store.book.model.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface CategoryRepository extends JpaRepository<Category, Long> {
 }
