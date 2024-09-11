de7392c9c7c08a3ef2d556af4a5e425165cf8ce9
Vitaliy <vitaliyfilimonov3@gmail.com>
2024-09-11 12:15:22 +0300

    refactor bookDto's

--- src/main/java/online/store/book/dto/book/CreateBookRequestDto.java
+++ src/main/java/online/store/book/dto/book/CreateBookRequestDto.java
@@ -4,6 +4,7 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
 import java.math.BigDecimal;
+import java.util.List;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -21,4 +22,5 @@ public class CreateBookRequestDto {
     private BigDecimal price;
     private String description;
     private String coverImage;
+    private List<Long> categoryIds;
 }
