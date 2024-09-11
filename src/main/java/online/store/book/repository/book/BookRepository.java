e0ad0a8cf92cb72510570e8abe83ec5ca23478f8
Vitaliy <vitaliyfilimonov3@gmail.com>
2024-09-11 17:38:43 +0300

    implemented findAllBooksByCategoryId

--- src/main/java/online/store/book/repository/book/BookRepository.java
+++ src/main/java/online/store/book/repository/book/BookRepository.java
@@ -1,7 +1,8 @@
 package online.store.book.repository.book;
 
-import java.util.List;
 import online.store.book.model.Book;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.jpa.repository.Query;
@@ -12,5 +13,5 @@ import org.springframework.stereotype.Repository;
 public interface BookRepository extends JpaRepository<Book, Long>,
         JpaSpecificationExecutor<Book> {
     @Query("SELECT b FROM Book b JOIN b.categories c WHERE c.id = :categoryId")
-    List<Book> findAllByCategoryId(@Param("categoryId") Long categoryId);
+    Page<Book> findAllByCategoryId(@Param("categoryId") Long categoryId, Pageable pageable);
 }
