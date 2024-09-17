59df3300214ce30edb556539a6e38578c92c19f8
Vitaliy <vitaliyfilimonov3@gmail.com>
2024-09-17 15:25:58 +0300

    refactor: RoleRepository and UserServiceImpl

--- src/main/java/online/store/book/repository/user/RoleRepository.java
+++ src/main/java/online/store/book/repository/user/RoleRepository.java
@@ -2,8 +2,12 @@ package online.store.book.repository.user;
 
 import online.store.book.model.Role;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface RoleRepository extends JpaRepository<Role, Long> {
+    @Query("SELECT r FROM Role r WHERE r.name = :name")
+    Role findByRoleName(@Param("name") Role.RoleName roleName);
 }
--- src/main/java/online/store/book/service/user/UserServiceImpl.java
+++ src/main/java/online/store/book/service/user/UserServiceImpl.java
@@ -1,14 +1,17 @@
 package online.store.book.service.user;
 
 import jakarta.transaction.Transactional;
+import java.util.Set;
 import lombok.RequiredArgsConstructor;
 import online.store.book.dto.user.UserRegistrationRequestDto;
 import online.store.book.dto.user.UserResponseDto;
 import online.store.book.exceptions.RegistrationException;
 import online.store.book.mapper.UserMapper;
+import online.store.book.model.Role;
 import online.store.book.model.ShoppingCart;
 import online.store.book.model.User;
 import online.store.book.repository.cart.ShoppingCartRepository;
+import online.store.book.repository.user.RoleRepository;
 import online.store.book.repository.user.UserRepository;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
@@ -20,6 +23,7 @@ public class UserServiceImpl implements UserService {
     private final UserMapper userMapper;
     private final ShoppingCartRepository shoppingCartRepository;
     private final PasswordEncoder passwordEncoder;
+    private final RoleRepository roleRepository;
 
     @Override
     @Transactional
@@ -31,6 +35,8 @@ public class UserServiceImpl implements UserService {
         }
         User user = userMapper.toUserEntity(requestUser);
         user.setPassword(passwordEncoder.encode(requestUser.getPassword()));
+        Role roleUser = roleRepository.findByRoleName(Role.RoleName.ROLE_USER);
+        user.setRoles(Set.of(roleUser));
         User userFromDb = userRepository.save(user);
         ShoppingCart shoppingCart = new ShoppingCart();
         shoppingCart.setUser(userFromDb);
