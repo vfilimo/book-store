230d1f609ec138d95c212e7e787830b765ff46cd
Vitaliy <vitaliyfilimonov3@gmail.com>
2024-09-19 12:15:05 +0300

    refactor: RoleRepository

--- src/main/java/online/store/book/repository/user/RoleRepository.java
+++ src/main/java/online/store/book/repository/user/RoleRepository.java
@@ -2,10 +2,7 @@ package online.store.book.repository.user;
 
 import online.store.book.model.Role;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 
 public interface RoleRepository extends JpaRepository<Role, Long> {
-    @Query("SELECT r FROM Role r WHERE r.name = :name")
-    Role findByRoleName(@Param("name") Role.RoleName roleName);
+    Role findByRoleName(Role.RoleName roleName);
 }
